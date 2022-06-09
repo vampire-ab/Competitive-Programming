4a3ed9f7a2e1999f2b328584897cc40c8f1389eb
vampire-ab <87767138+vampire-ab@users.noreply.github.com>
2022-06-09 23:33:53 +0530

    Create 734A.cpp

---
+++ CodeForces/734A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, a = 0, d = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for(int i = 0; i < n; i++){
+        if(s[i] == 'A'){
+            a++;
+        }
+        else{
+            d++;
+        }
+    }
+    if(a > d)
+    cout << "Anton";
+    else if(d > a)
+    cout << "Danik";
+    else{
+        cout << "Friendship";
+    }
+}
