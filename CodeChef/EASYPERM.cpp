fdee4e58f0802116c2a5789781711b691a88e512
vampire-ab <brijeshagal@gmail.com>
2022-09-02 12:33:08 +0530

    Add files via upload

---
+++ CodeChef/EASYPERM.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int n;
+
+void solve()
+{
+  cin >> n;
+  while (n)
+  {
+    cout << n << " ";
+    n--;
+  }
+  cout << endl;
+}
+
+int main()
+{
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
